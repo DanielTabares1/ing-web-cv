6d3ee87534d6c5c6c29058d54fb56621984c684f
Daniel Tabares <daniel.tabares1@udea.edu.co>
2023-09-17 10:12:01 -0500

    Added Links Icons

--- components/Links.tsx
+++ components/Links.tsx
@@ -1,9 +1,19 @@
-
+import { Icon } from "./Icon";
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaYoutube } from "react-icons/fa";
+import { PiInstagramLogoFill, PiDevToLogoFill } from "react-icons/pi";
 
 const Links = () => {
     return(
-        <div className="h-screen w-20 bg-white">
 
+        
+        <div className="h-screen w-20 bg-white flex flex-col items-center gap-3 p-4">
+            <h4 className="font-semibold">Links</h4>            
+            <Icon Icon={FaFacebookF} src="https://www.google.com"/>
+            <Icon Icon={PiInstagramLogoFill} src="https://www.google.com"/>
+            <Icon Icon={FaTwitter} src="https://www.google.com"/>
+            <Icon Icon={FaLinkedinIn} src="https://www.google.com"/>
+            <Icon Icon={FaYoutube} src="https://www.google.com"/>
+            <Icon Icon={PiDevToLogoFill} src="https://www.google.com"/>            
         </div>
     );
 };
